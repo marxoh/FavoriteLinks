9aab6424d4d616355f0a59dccbbefe09b20512eb
Marcos Mayorga <marxoh22@gmail.com>
2023-04-29 14:03:26 -0400

    hotfix002 solucionado

--- src/routes/links.js
+++ src/routes/links.js
@@ -9,24 +9,26 @@ const pool = require("../database");
 
 //express.router.get: para cuando el navegador trate de pedir una peticion get al servidor a la ruta /add
 //se va a responder de la siguiente mnaera
-router.get("/add", (req, res) => { //con req y res se encargan de manejar esta peticion
+router.get("/add", (req, res) => {
+  //con req y res se encargan de manejar esta peticion
   //res.send("Form");
   res.render("links/add");
 });
 
 //submit del formulario
 //tienen el mismo nombre "/add" pero se van a diferenciar por el metodo http post get
-router.post('/add',(req,res)=>{
-    // res.send('Recibido')//solo muestra la palabra recibido originalmete
-    //console.log(req.body)//muestra como recibimos los datos del formulario
-    //para tenerlo mas ordenado
-    const {title,url,description} = req.body
-    //para vincularlo luego al usuario que lo guardo?
-    const newLink = {
-        title,
-        url,
-        description
-    }
-})
+router.post("/add", (req, res) => {
+  //console.log(req.body)//muestra como recibimos los datos del formulario
+  //para tenerlo mas ordenado
+  const { title, url, description } = req.body;
+  //para vincularlo luego al usuario que lo guardo?
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  console.log(newLink);
+  res.send("Recibido"); //solo muestra la palabra recibido
+});
 
 module.exports = router;
